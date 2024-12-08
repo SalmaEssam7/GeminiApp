d96e75e103312a788ab952470d67b657c15504c6
SalmaEssam7 <Sees7878@gmail.com>
2024-12-08 19:29:58 +0200

    Initial commit

---
+++ GeminiApp/src/App.jsx
@@ -0,0 +1,16 @@
+
+// import './App.css'
+// Created by Salma Essam  on 1/12/2024. 
+import Main from './Components/MainCom/Main'
+import SideBar from './Components/SideBarCom/SideBar'
+function App() {
+
+  return (
+   <>
+   <SideBar />
+   <Main/>
+   </>
+  )
+}
+
+export default App
