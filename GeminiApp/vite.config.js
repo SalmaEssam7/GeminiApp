9100f9378905ed7f77a20c06c1a19641088b4b80
SalmaEssam7 <Sees7878@gmail.com>
2024-12-27 20:50:28 +0200

    Initial commit

--- GeminiApp/vite.config.js
+++ GeminiApp/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
 // export default defineConfig({
-//   base: '/GeminiApp/', // تأكد من أن المسار يتطابق مع اسم المستودع
-//   plugins: [react()]
+//   plugins: [react()],
 // })
+
+export default defineConfig({
+  base: '/GeminiApp/', // تأكد من أن المسار يتطابق مع اسم المستودع
+  plugins: [react()]
+})
